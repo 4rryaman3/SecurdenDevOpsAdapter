c18e1c13db138e2c15c69534a663948752a4a398
Shree Arryaman AS <shreearryaman@securden.com>
2025-05-09 03:07:51 +0530

    securden devops adapter protocol

---
+++ SecurdenBaseApi.cs
@@ -0,0 +1,83 @@
+﻿using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Net.Http.Json;
+using System.Text.Json;
+
+namespace SecurdenApiProtocol
+{
+
+    public interface ISecurdenApiClient : IDisposable
+    {
+        T? GetAsync<T>(string endpoint, IEnumerable<KeyValuePair<string, string>>? a);
+        T? PostFormdata<T>(string endpoint, IEnumerable<KeyValuePair<string, string>> formData);
+    }
+
+    public class BaseApiClient : ISecurdenApiClient
+    {
+        private HttpClient _httpClient;
+        private string _authToken = "";
+        private bool _disposed = false;
+        public BaseApiClient(string baseUrl, string authToken)
+        {
+            if (string.IsNullOrWhiteSpace(baseUrl))
+                throw new ArgumentNullException(nameof(baseUrl));
+
+            if (string.IsNullOrWhiteSpace(authToken))
+                throw new ArgumentNullException(nameof(authToken));
+
+            _authToken = authToken;
+            var handler = new HttpClientHandler
+            {
+                AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate
+            };
+
+            _httpClient = new HttpClient(handler)
+            {
+                BaseAddress = new Uri(baseUrl),
+            };
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.DefaultRequestHeaders.Add("authtoken", _authToken);
+        }
+        public T? GetAsync<T>(string endpoint, IEnumerable<KeyValuePair<string, string>>? queryParams)
+        {
+
+
+            if (queryParams != null && queryParams.Any())
+            {
+                var query = string.Join("&", queryParams.Select(kvp => $"{WebUtility.UrlEncode(kvp.Key)}={WebUtility.UrlEncode(kvp.Value)}"));
+                endpoint += endpoint.Contains('?') ? "&" + query : "?" + query;
+            }
+
+            var response = _httpClient.GetAsync(endpoint).GetAwaiter().GetResult();
+            response.EnsureSuccessStatusCode();
+
+            var json = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+            return JsonSerializer.Deserialize<T>(json);
+        }
+        public T? PostFormdata<T>(string endpoint, IEnumerable<KeyValuePair<string, string>> formData)
+        {
+            var content = new FormUrlEncodedContent(formData);
+            var response = _httpClient
+            .PostAsync(endpoint, content)
+            .GetAwaiter()
+            .GetResult();
+            response.EnsureSuccessStatusCode();
+
+            var json = response.Content
+             .ReadAsStringAsync()
+             .GetAwaiter()
+             .GetResult();
+
+            return JsonSerializer.Deserialize<T>(json);
+        }
+        
+        public void Dispose()
+        {
+            _disposed = true;
+            ((IDisposable)_httpClient).Dispose();
+        }
+    }
+}
---
+++ SecurdenDevOpsAdapter.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Text.Json.Serialization;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Text;
+using System.ComponentModel;
+
+namespace SecurdenApiProtocol
+{
+    public class AccountDto
+    {
+        [JsonPropertyName("account_id")]
+        public long? AccountId { get; set; }
+
+        [JsonPropertyName("account_name")]
+        public string? AccountName { get; set; }
+
+        [JsonPropertyName("account_title")]
+        public string? AccountTitle { get; set; }
+
+        [JsonPropertyName("password")]
+        public string? Password { get; set; }
+
+        [JsonPropertyName("address")]
+        public string? Address { get; set; }
+
+        [JsonPropertyName("port")]
+        public string? Port { get; set; }
+
+        [JsonPropertyName("status_code")]
+        public int? StatusCode { get; set; }
+
+        [JsonPropertyName("message")]
+        public string? Message { get; set; }
+
+        [JsonExtensionData]
+        public Dictionary<string, JsonElement>? additionalData { get; set; }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            sb.Append("{");
+
+            if (AccountId != null) sb.Append($"\"account_id\": {AccountId},");
+            if (!string.IsNullOrEmpty(AccountName)) sb.Append($"\"account_name\": \"{AccountName}\",");
+            if (!string.IsNullOrEmpty(AccountTitle)) sb.Append($"\"account_title\": \"{AccountTitle}\",");
+            if (sb[sb.Length - 1] == ',')
+                sb.Length--;
+            sb.Append("}");
+            return sb.ToString();
+        }
+
+    }
+    public class GetAccountsResponse
+    {
+        [JsonPropertyName("status_code")]
+        public int? StatusCode { get; set; }
+
+        [JsonPropertyName("message")]
+        public string? Message { get; set; }
+
+        [JsonExtensionData]
+        public Dictionary<string, JsonElement>? AccountsData { get; set; }
+    }
+
+    public class SecurdenApiAdapter : IDisposable       
+    {
+   
+        private readonly BaseApiClient _baseClient;
+        public readonly List<string> fields = ["account_id", "account_name", "account_title", "account_type", "ticket_id","reason"];
+        public SecurdenApiAdapter(string baseUrl, string authToken)
+        {
+            _baseClient = new BaseApiClient(baseUrl, authToken);
+        }
+        public AccountDto? GetSpecificAccount(List<KeyValuePair<string, string>>? accountParams = null)
+        {
+            var filteredParams = new List<KeyValuePair<string, string>>();
+
+            if (accountParams != null)
+            {
+                filteredParams = accountParams
+                    .Where(kvp => fields.Contains(kvp.Key))
+                    .ToList();
+            }
+
+            var returnObj = _baseClient.GetAsync<AccountDto>("/secretsmanagement/get_account", filteredParams);
+            return returnObj;
+        }
+        public List<AccountDto> GetAccounts(List<string>? accountIdsList=null, List<Dictionary<string, string>>? accountParams=null)
+        {
+            var returnObj = new List<AccountDto>();
+
+            if (accountIdsList == null || accountIdsList.Count == 0)
+                return returnObj;
+
+            var accountIdParams = "[" + string.Join(",", accountIdsList) + "]";
+
+            var filteredParamsList = new List<Dictionary<string, string>>();
+            if (accountParams != null)
+            {
+                filteredParamsList = accountParams
+                    .Select(dict => dict
+                        .Where(kvp => fields.Contains(kvp.Key))
+                        .ToDictionary(kvp => kvp.Key, kvp => kvp.Value))
+                    .ToList();
+            }
+
+            var accountsJson = JsonSerializer.Serialize(filteredParamsList);
+            var formData = new List<KeyValuePair<string, string>>
+                {
+                    new("account_ids", accountIdParams),
+                    new("accounts", accountsJson)
+                };
+
+            var accountResponse = _baseClient.PostFormdata<GetAccountsResponse>("/secretsmanagement/get_accounts", formData);
+
+            if (accountResponse?.AccountsData != null)
+            {
+                foreach (var kvp in accountResponse.AccountsData)
+                {
+                    var dto = kvp.Value.Deserialize<AccountDto>();
+                    if (dto != null)
+                        returnObj.Add(dto);
+                }
+            }
+
+            return returnObj;
+        }
+        public void Dispose()
+        {
+            _baseClient.Dispose();
+        }
+
+     
+    }
+}
